53fefdbeb4837ceea973c94e786291d81dbe8e57
MalwandlaShaun <hlungwaniamlwandlashaun@gmail.com>
2023-06-02 10:28:56 +0200

    final push

---
+++ src/App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     
+    <div className='top-divs'>
+      <div className='top-div-one'></div>
+      <div className='top-div-two'></div>
+      <div className='top-div-three'></div>
+    </div>
+    <div className='mid-divs'>
+      <div className='mid-div-one'></div>
+      <div className='mid-div-two'>
+      </div>
+      <div className='mid-div-three'></div>
+    </div>
+    
+    <div className='bott-div'></div>
+    </div>
+  );
+}
+
+export default App;
